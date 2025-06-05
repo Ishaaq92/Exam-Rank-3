336ab884be72321b6155cfff26fa56b554885110
Ishaaq Ahmed <a.ishaaq@icloud.com>
2025-06-05 15:46:39 +0100

    Almost done just need to fix the bug dealing with the last line

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ishaaq <ishaaq@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 18:36:25 by ishaaq            #+#    #+#             */
+/*   Updated: 2025/06/05 14:47:30 by ishaaq           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+#define BUFFER_SIZE 1 
+#endif
