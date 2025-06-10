a3fae13e2abd2e5107012e4d3c935937120943be
Ishaaq Ahmed <a.ishaaq@icloud.com>
2025-06-10 15:21:18 +0100

    Most recent changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,83 +6,12 @@
 /*   By: ishaaq <ishaaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 14:30:39 by ishaaq            #+#    #+#             */
-/*   Updated: 2025/06/05 16:08:42 by ishaaq           ###   ########.fr       */
+/*   Updated: 2025/06/09 21:07:43 by ishaaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_strnchr(char *s, char c)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0' && s[i] != c)
-		i ++;
-	if (s[i] == c)
-		return (i);
-	return (0);
-}
-
-char    *ft_strchr(char *s, char c)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0' && s[i] != c)
-		i ++;
-	if (s[i] == c)
-		return (&s[i]);
-	return (0);
-}
-
-int ft_strlen(char *str)
-{
-	int i;
-	
-	i = 0;
-	while (str[i] != '\0')
-		i ++;
-	return (i);
-}
-
-char    *ft_strjoin(char *s1, char *s2)
-{
-	char    *new;
-	int     i;
-	int     s1_len;
-
-	if (!s1 || !s2)
-		return (NULL);
-	s1_len = ft_strlen(s1);
-	new = malloc(sizeof(char) * (s1_len + ft_strlen(s2) + 1));
-	if (!new)
-		return (NULL);
-	i = -1;
-	while (s1[++i] != '\0')
-		new[i] = s1[i];
-	i = -1;
-	while (s2[++i] != '\0')
-		new[s1_len + i] = s2[i];
-	new[s1_len + i] = '\0';
-	return (new);
-}
-
-char    *ft_strndup(char *str, int bytes)
-{
-	int		i;
-	char	*new;
-
-	i = 0;
-	new = malloc(sizeof(char) * (bytes + 1));
-	while (str[i] != '\0' && i < bytes)
-	{
-		new[i] = str[i];
-		i ++;        
-	}
-	new[i] = '\0';
-	return (new);
-}
 
 void	shift_bytes_line(char *line)
 {
@@ -98,30 +27,30 @@ void	shift_bytes_line(char *line)
 
 void	shift_bytes_buffer(char *line, char *buffer)
 {
-	char	*new;
-
 	if (line)
 		free(line);
-	line = ft_strndup(ft_strchr(buffer, '\n') + 1, ft_strlen(ft_strchr(buffer, '\n')));
-	// printf("remaining bytes = \'%s\'" , *line);
+	line = ft_strndup(ft_strchr(buffer, '\n') + 1, ft_strlen(ft_strchr(buffer, '\n') + 1));
 }
 
-// Just appends the full BUFFER_SIZE number of characters and gets returned. 
-char	*append_characters(char *line, char *buffer)
+// Just appends the full num_bytes number of characters and gets returned. 
+char	*append_characters(char *line, char *buffer, int num_bytes)
 {
 	char	*new;
 	char	*tmp;
 
-	// printf("not nl");
+	printf("not nl");
 	if (!line)
 	{
-		line = ft_strndup(buffer, BUFFER_SIZE);
+		line = ft_strndup(buffer, num_bytes);
+		// printf("debugging = %s", new);
 		return (line);
 	}
-	tmp = ft_strndup(buffer, BUFFER_SIZE);
+	tmp = ft_strndup(buffer, num_bytes);
+	printf("tmp = %s", tmp);
 	new = ft_strjoin(line, tmp);
 	if (tmp)
 		free(tmp);
+	printf("debugging = %s\n", new);
 	return (new);
 }
 
@@ -133,20 +62,20 @@ char	*append_characters_nl(char *line, char *buffer, int bytes)
 
 	if (!(line))
 	{
-		new = ft_strndup(buffer, ft_strnchr(buffer, '\n') + 1);
+		new = ft_strndup(buffer, bytes + 1);
 		shift_bytes_buffer(line, buffer);
 		return (new);
 	}
-	temp = ft_strndup(buffer, ft_strnchr(buffer, '\n') + 1);
+	temp = ft_strndup(buffer, bytes + 1);
 	new = ft_strjoin(line, temp);
-	free(temp);
+	if (temp)
+		free(temp);
 	shift_bytes_buffer(line, buffer);
 	return (new);
 }
 
 char	*check_remaining_bytes_nl(char *line)
 {
-	int		i;
 	char	*new;
 
 	if (!line || ft_strchr(line, '\n') == NULL)
@@ -178,16 +107,18 @@ char    *get_next_line(int fd)
 		return (NULL);
 	if (line && ft_strchr(line, '\n'))
 		return (check_remaining_bytes_nl(line));
+	printf("here\n");
 	while (42)
 	{
 		num_bytes = read(fd, buffer, BUFFER_SIZE);
 		if (!num_bytes)
 			return (halt(line));
 		buffer[num_bytes] = '\0';
+		printf("%d, %s\n", num_bytes, buffer);
 		if (ft_strchr(buffer, '\n'))
 			return (append_characters_nl(line, buffer, ft_strnchr(buffer, '\n')));
 		else
-			line = append_characters(line, buffer);
+			line = append_characters(line, buffer, num_bytes);
 	}
 	return (line);
 }
@@ -197,15 +128,17 @@ int main(int ac, char **av)
 	int 	fd;
 	char	*line;
 
+	(void) ac;	
 	if (!access(av[1], O_RDONLY))
 		fd = open(av[1], O_RDONLY);
 	else
 		fd = open("file.txt", O_RDONLY);
-	line = get_next_line(fd);
-	// printf("%s", line);
-	while (line != NULL)
-	{
-		printf("%s", line);
-		line = get_next_line(fd);
-	}
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	// line = get_next_line(fd);
+	// while (line != NULL)
+	// {
+	// 	printf("%s", line);
+	// 	line = get_next_line(fd);
+	// }
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ishaaq <ishaaq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 18:36:25 by ishaaq            #+#    #+#             */
-/*   Updated: 2025/06/05 14:47:30 by ishaaq           ###   ########.fr       */
+/*   Updated: 2025/06/06 17:50:36 by ishaaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,16 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-#define BUFFER_SIZE 1 
+#define BUFFER_SIZE 3 
+
+int	    ft_strnchr(char *s, char c);
+char    *ft_strchr(char *s, char c);
+int     ft_strlen(char *str);
+char    *ft_strjoin(char *s1, char *s2);
+char    *ft_strndup(char *str, int bytes);
+
+// public
+char    *get_next_line(int fd);
+
+
 #endif
